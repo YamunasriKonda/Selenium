f9761e2b5684df546478e32d2eb937643849f705
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-10-21 11:40:35 +0100

    https://github.com/YamunasriKonda/Selenium.git

--- tests/exercise.test.js
+++ tests/exercise.test.js
@@ -10,7 +10,7 @@ describe("selenium tests", function () {
     try {
       await driver.get("https://www.bbc.co.uk/search");
       const search = await driver.findElement(By.id("search-input"));
-      await search.sendKeys("turtles");
+      await search.sendKeys("value", Key.ENTER);
       await driver.findElement(By.css("#main-content > form > div > div > div > button")).click();
       const result = await driver.findElement(By.css("#main-content > div.ssrcss-1v7bxtk-StyledContainer.enjd40x0 > div > div > ul > li:nth-child(1) > div > div > div.ssrcss-tq7xfh-PromoContent.e1f5wbog7 > div.ssrcss-1f3bvyz-Stack.e1y4nx260 > a > span > p > span"));
       expect(await result.getText()).to.equal("The Man Who Made Mock Turtle Soup");
